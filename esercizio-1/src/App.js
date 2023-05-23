446bdf278258be5689333248707625ad17d311ee
Alessandro <al3ss4ndr@hotmail.it>
2023-05-23 15:48:39 +0200

    Ex 1 completed

--- esercizio-1/src/App.js
+++ esercizio-1/src/App.js
@@ -1,8 +1,12 @@
+import { Hello } from "./Components/Hello";
+import { Message } from "./Components/Message";
 
 function App() {
   return (
-   <>
-   </>
+    <>
+      <Hello />
+      <Message />
+    </>
   );
 }
 
---
+++ esercizio-1/src/Components/Message.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export class Message extends React.Component{
+    render(){
+       return(
+        <h2>It's time to learn React!!!</h2>
+       )
+    }
+}
