3d1d834efed444ed5799995afb41d00c0a61d028
Alessandro <al3ss4ndr@hotmail.it>
2023-05-24 16:10:55 +0200

    Exe 8 completed

---
+++ esercizio-8/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Welcome } from './Components/Welcome'
+
+
+
+function App() {
+  return (
+    <>
+       <Welcome/>
+    </>
+  )
+}
+
+export default App
---
+++ esercizio-8/src/Components/Age.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+export class Age extends React.Component {
+    render() {
+        return (
+            <div>
+                {/* {this.props.age 
+                    ? <p>Your age is {this.props.age}</p>
+                    : <p>Your age is lower than 18 </p>
+                } */}
+
+                {this.props.age && <p>Your age is: {this.props.age}</p>}
+            </div>
+        )
+
+    }
+}
