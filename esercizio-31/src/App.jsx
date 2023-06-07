9fa19cd26f2540113157eb8422b3f4e45211d0fb
Alessandro <al3ss4ndr@hotmail.it>
2023-06-07 14:10:10 +0200

    exe 31

---
+++ esercizio-31/src/App.jsx
@@ -0,0 +1,13 @@
+import { Login } from "./Components/Login"
+
+function App() {
+  
+
+  return (
+    <>
+     <Login/>
+    </>
+  )
+}
+
+export default App
---
+++ esercizio-31/src/Components/Login.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+
+
+export function Login() {
+    const [data, setData] = useState({
+        username: "",
+        password: "",
+        remember: false
+    })
+
+    function handleInputChange(event){
+        const  {name, type, value, checked} = event.target
+
+        setData((prevData) => ({
+            ...prevData,
+            [name]: type === "checkbox" ? checked : value,
+          }));
+          console.log(data)
+    }
+
+    return (
+        <form>
+            <input onChange={handleInputChange} name="username" value={data.username}/>
+            <input onChange={handleInputChange} name="password" value={data.password} type="password"/>
+            <input onChange={handleInputChange} name="remember" value={data.remember} type="checkbox" />
+        </form>
+    )
+}
