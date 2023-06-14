9f6e3e93573b6f811a022178bf47e15b502f424b
Alessandro <al3ss4ndr@hotmail.it>
2023-06-14 16:11:43 +0200

    exe 35 fixed

--- esercizio-35/src/components/GithubUserList.jsx
+++ esercizio-35/src/components/GithubUserList.jsx
@@ -26,10 +26,9 @@ function GithubUserList() {
     <div>
         <ul>
             {users.map((element,index) => {
-                return <li key={index}>{element}</li> 
+                return <GithubUser onAddUser={handleAddUser}/>
             })}
         </ul>
-        <GithubUser onAddUser={handleAddUser}/>
     </div>
   )
 }
