ad3d94144c43a914df6ab92c03176098950b25a8
Alessandro <al3ss4ndr@hotmail.it>
2023-06-19 14:43:17 +0200

    exe 43 fixed, 51-52

--- esercizio-43/src/components/FiltredList.jsx
+++ esercizio-43/src/components/FiltredList.jsx
@@ -1,26 +1,30 @@
 import React from 'react'
-import { useMemo } from 'react'
 import { useState } from 'react'
 import { useEffect } from 'react'
+import { useMemo } from 'react'
 
-function FiltredList({arg}) {
+
+function Component({arg}) {
     const [arr, setArr] = useState([])
 
-  useEffect(() => {
-   setArr(arg.filter((element) => {
-    return element.age > 18
-   }))
-  }, [arg])
+    useEffect(() => {
+      setArr(arg)}, [arg])
 
-  const memoizedArr = useMemo(() => arr,[arr])
-    
-    return (
-        <div>
-             {arr.map((element, index) => {
-                return <li key={index}>name: {element.name} id: {element.id} age:{element.age}</li>
-             })}
-        </div>
-    )
+      const memoizedArr = useMemo(() => {
+        return arr.filter((element) => {
+          return element.age > 18
+        })
+      }, [arr])
+
+  return (
+    <div>
+        {
+            memoizedArr.map((element, index) => {
+                return <p key={index}>name: {element.name} <br /> age: {element.age}</p>
+            })
+        }
+    </div>
+  )
 }
 
-export default FiltredList
+export default Component
